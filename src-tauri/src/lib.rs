27cc482ec5dda80dd1e8b10447a73f9c4593846d
Ab <abdelilah@alfred.com>
2025-06-19 10:44:34 +0200

    added sqlite

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,7 +8,25 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create_initial_tables",
+            sql: "CREATE TABLE IF NOT EXISTS workshops (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    address TEXT NOT NULL,
+                    vat_number TEXT NOT NULL,
+                    phone TEXT NOT NULL,
+                    email TEXT NOT NULL,
+                    base_labor_cost DECIMAL(10, 2) NOT NULL
+                    );",
+            kind: MigrationKind::Up,
+        }
+    ];
+
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::default().add_migrations("sqlite:estimastes.db", migrations).build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
