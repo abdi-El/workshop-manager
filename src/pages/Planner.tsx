8da636f17692642377c731af24058dca55096e42
Ab <abdila1099@gmail.com>
2025-07-03 17:08:07 +0200

    added query

--- src/pages/Planner.tsx
+++ src/pages/Planner.tsx
@@ -1,9 +1,29 @@
+import { EventImpl } from '@fullcalendar/core/internal';
 import itLocale from '@fullcalendar/core/locales/it';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import { useMemo } from 'react';
-import { useDatabaseStore } from '../modules/state';
-import { Appointment } from '../types/database';
+import { Tooltip } from 'antd';
+import { useEffect, useMemo, useState } from 'react';
+import { db } from '../modules/database';
+
+interface AppointmentEventData {
+    id: number,
+    workshop_id: number,
+    date: string,
+    from_time: string,
+    to_time: string,
+    customer_name: string,
+    customer_phone: string,
+    car_info: string,
+    number_plate: string,
+    estimate_status: boolean
+}
+
+interface EventProps extends EventImpl {
+    extendedProps: {
+        appointment: AppointmentEventData
+    }
+}
 
 function toISOFormat(dateString: string, timeString: string) {
     const [day, month, year] = dateString.split('-');
@@ -12,23 +32,59 @@ function toISOFormat(dateString: string, timeString: string) {
     return isoDateTime;
 }
 
-const mapAppointmentsToEvents = (appointments: Appointment[]) => {
+const mapAppointmentsToEvents = (appointments: AppointmentEventData[]) => {
     return appointments.map((appt) => {
         return {
             id: appt.id.toString(),
             title: `Appt #${appt.id}`,
             start: toISOFormat(appt.date, appt.from_time),
             end: toISOFormat(appt.date, appt.to_time),
+            extendedProps: {
+                appointment: appt,
+            }
         }
     });
 };
 
 export default function Planner() {
-    const { appointments } = useDatabaseStore(state => state);
+    const [appointments, setAppointments] = useState<AppointmentEventData[]>([])
 
     const events = useMemo(() => {
         return appointments ? mapAppointmentsToEvents(appointments) : [];
     }, [appointments]);
+
+    useEffect(() => {
+        db.select(
+            `SELECT 
+                    a.id as id,
+                    a.workshop_id,
+                    a.date,
+                    a.from_time,
+                    a.to_time,
+                    
+                    -- Customer (conditional)
+                    c.name as customer_name,
+                    c.phone as customer_phone,
+                    
+                    -- Car (conditional)
+                    CONCAT(maker.name, ' ', model.name, ' (', car.year, ')') as car_info,
+                    car.number_plate,
+                    
+                    -- Estimate status
+                    CASE 
+                        WHEN a.estimate_id IS NOT NULL THEN 1
+                        ELSE 0
+                    END as estimate_status
+
+                FROM appointments a
+                LEFT JOIN estimates e ON a.estimate_id = e.id
+                LEFT JOIN customers c ON COALESCE(e.customer_id, a.customer_id) = c.id
+                LEFT JOIN cars car ON COALESCE(e.car_id, a.car_id) = car.id
+                LEFT JOIN makers maker ON car.maker_id = maker.id
+                LEFT JOIN models model ON car.model_id = model.id
+                ORDER BY a.date DESC, a.from_time;`
+        ).then((res) => setAppointments(res as any))
+    }, [])
 
 
     return (
@@ -42,6 +98,14 @@ export default function Planner() {
             }}
             locale={itLocale}
             events={events}
+            eventContent={(eventInfo) => {
+                const { event } = eventInfo;
+                const { extendedProps } = event as EventProps;
+                const { appointment } = extendedProps
+                return <Tooltip title="prompt text">
+                    <div>{appointment.car_info} di {appointment.customer_name}</div>
+                </Tooltip>
+            }}
         />
     );
 }
