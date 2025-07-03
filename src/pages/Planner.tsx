46ba3d0a56b80a39b2aa5f41020dd7918339d6d0
Ab <abdila1099@gmail.com>
2025-07-03 12:53:36 +0200

    added planner

---
+++ src/pages/Planner.tsx
@@ -0,0 +1,19 @@
+import itLocale from '@fullcalendar/core/locales/it';
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+export default function Planner() {
+    return (
+        <FullCalendar
+            plugins={[timeGridPlugin]}
+            initialView='timeGridWeek'
+            headerToolbar={{
+                left: 'prev,next',
+                center: 'title',
+                right: 'timeGridWeek,timeGridDay'
+            }}
+            locale={itLocale}
+
+        />
+    )
+}
