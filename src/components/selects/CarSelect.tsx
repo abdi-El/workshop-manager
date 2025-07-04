34eb1f079a4abc36256f4ac617f34f22402a0d65
Ab <abdila1099@gmail.com>
2025-07-04 16:52:32 +0200

    fixed type

--- src/components/selects/CarSelect.tsx
+++ src/components/selects/CarSelect.tsx
@@ -3,5 +3,5 @@ import DatabasResourceSelect from "./DatabaseResourceSelect";
 interface Props extends React.ComponentProps<typeof Form.Item> { }
 
 export default function CarSelect(props: Props) {
-    return <DatabasResourceSelect resource="cars" selectLabel="id" name="car_id" inputLabel="Auto" {...props} />
+    return <DatabasResourceSelect {...props} resource="cars" selectLabel="id" name="car_id" inputLabel="Auto" />
 }
