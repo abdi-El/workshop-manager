f38864c2ebe4c1c71aec3f2b8ad53ea668f8f616
Ab <abdila1099@gmail.com>
2025-07-29 15:24:58 +0200

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ export default function Page() {
     await initDatabase()
     updateSettings()
     updateDatabaseData()
-    if (!makers?.length) {
+    if (makers.length) {
       trigger()
     } else {
       setPercentage(100)
