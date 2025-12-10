655bd72e9ba33060d8e27e58d36c73c1fd1bc681
Ab <abdila1099@gmail.com>
2025-12-10 15:15:55 +0100

    feat: add InspectionReminder component to display upcoming inspections

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import CarsAVGS from "../components/dashboard/CarsAVGS";
 import EstimatesAVGS from "../components/dashboard/EstimatesAVGS";
+import InspectionReminder from "../components/dashboard/InspectionReminder";
 
 export default function Dashboard() {
     return <>
         <EstimatesAVGS />
         <CarsAVGS />
+        <InspectionReminder style={{ marginTop: 20 }} />
     </>
 }
 
