6869b7cbfed5408ec7761e5d5377acd31b97f01f
Ab <abdila1099@gmail.com>
2026-03-28 00:15:30 +0100

    feat: conditionally render RevenueChart and TopCustomers based on showRevenueStatistics setting

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,19 @@
 import CarsAVGS from "../components/dashboard/CarsAVGS";
 import EstimatesAVGS from "../components/dashboard/EstimatesAVGS";
 import InspectionReminder from "../components/dashboard/InspectionReminder";
+import RevenueChart from "../components/dashboard/RevenueChart";
+import TopCustomers from "../components/dashboard/TopCustomers";
+import { useStore } from "../modules/state";
 
 export default function Dashboard() {
+    const { settings } = useStore((state) => state)
+
     return <>
         <EstimatesAVGS />
-        {/* <RevenueChart /> */}
-        {/* <TopCustomers /> */}
+        {settings.showRevenueStatistics && <>
+            <RevenueChart />
+            <TopCustomers />
+        </>}
         <CarsAVGS />
         <InspectionReminder style={{ marginTop: 20 }} />
     </>
